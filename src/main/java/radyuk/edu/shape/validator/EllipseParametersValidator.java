64731c744c02592bf5ca8e4b5f0a21063d015652
Karina Radyuk <radyuk.karina@bk.ru>
2021-11-20 20:48:28 +0300

    fix: change areParametersValid method parameters

--- src/main/java/radyuk/edu/shape/validator/EllipseParametersValidator.java
+++ src/main/java/radyuk/edu/shape/validator/EllipseParametersValidator.java
@@ -1,13 +1,13 @@
 package radyuk.edu.shape.validator;
 
-import radyuk.edu.shape.entity.Ellipse;
+import radyuk.edu.shape.entity.Point;
 
 public class EllipseParametersValidator {
-    public static boolean areParametersValid(Ellipse ellipse) {
-        if (ellipse.getFirstPoint() == null || ellipse.getSecondPoint() == null) {
+    public static boolean areParametersValid(Point firstPoint, Point secondPoint) {
+        if (firstPoint == null || secondPoint == null) {
             return false;
         }
-        return ellipse.getFirstPoint().x() != ellipse.getSecondPoint().x()
-                && ellipse.getFirstPoint().y() != ellipse.getSecondPoint().y();
+        return firstPoint.x() != secondPoint.x()
+                && firstPoint.y() != secondPoint.y();
     }
 }
