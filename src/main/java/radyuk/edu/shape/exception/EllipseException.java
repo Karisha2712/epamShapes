db18f4452d8b4bff32f1f02e3ee0922d640814b6
Karina Radyuk <radyuk.karina@bk.ru>
2021-11-20 20:42:00 +0300

    feat: add Ellipse exception class

---
+++ src/main/java/radyuk/edu/shape/exception/EllipseException.java
@@ -0,0 +1,20 @@
+package radyuk.edu.shape.exception;
+
+public class EllipseException extends Exception {
+    public EllipseException() {
+
+    }
+
+    public EllipseException(String message) {
+        super(message);
+    }
+
+    public EllipseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public EllipseException(Throwable cause) {
+        super(cause);
+    }
+
+}
