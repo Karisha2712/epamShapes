924dfdcd7e1b803ff6f9cf3cdda805de8913b759
Karina Radyuk <radyuk.karina@bk.ru>
2021-11-21 13:43:26 +0300

    feat: add EllipseFileReader and EllipseFileLineValidator classes

---
+++ src/main/java/radyuk/edu/shape/reader/EllipseFileReader.java
@@ -0,0 +1,31 @@
+package radyuk.edu.shape.reader;
+
+import radyuk.edu.shape.exception.EllipseException;
+import radyuk.edu.shape.validator.EllipseFileLineValidator;
+import radyuk.edu.shape.validator.EllipseFileValidator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EllipseFileReader {
+    private static final String FILE_ERROR = "Invalid file path";
+
+    public List<String> getValidLines(String filepath) throws EllipseException {
+        if (!EllipseFileValidator.isFileValid(filepath)) {
+            throw new EllipseException(FILE_ERROR);
+        }
+        Path path = Paths.get(filepath);
+        try (Stream<String> fileLines = Files.lines(path)) {
+            return fileLines
+                    .filter(EllipseFileLineValidator::isEllipseFileLineValid)
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new EllipseException("");
+        }
+    }
+}
